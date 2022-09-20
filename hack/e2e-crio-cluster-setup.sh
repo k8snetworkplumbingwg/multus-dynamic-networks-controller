6413897a556f54e2f09fc298b7283129b7ce645d
Miguel Duarte Barroso <mdbarroso@redhat.com>
2022-09-20 10:25:20 +0200

    ci, e2e tests: provide crio target
    minikube 1.26.1 has CRI-O 1.24.X according to its release notes - [0].
    
    [0] - https://github.com/kubernetes/minikube/releases/tag/v1.26.1
    
    Signed-off-by: Miguel Duarte Barroso <mdbarroso@redhat.com>

--- hack/e2e-crio-cluster-setup.sh
+++ hack/e2e-crio-cluster-setup.sh
@@ -10,47 +10,34 @@ IMAGE_REPO=${IMG_REPO:-maiqueb}
 IMAGE_NAME="multus-dynamic-networks-controller"
 IMAGE_TAG="latest"
 
-setup_cluster() {
-    export KUBEVIRTCI_TAG=`curl -L -Ss https://storage.googleapis.com/kubevirt-prow/release/kubevirt/kubevirtci/latest`
-    git clone https://github.com/kubevirt/kubevirtci/
-    trap "popd" RETURN SIGINT
-    pushd kubevirtci
-    KUBEVIRT_PROVIDER="k8s-$K8S_VERSION" KUBEVIRT_NUM_SECONDARY_NICS=1 make cluster-up
-    export KUBECONFIG="$(pwd)/_ci-configs/k8s-1.24/.kubeconfig"
-}
-
 push_local_image() {
     OCI_BIN="$OCI_BIN" IMAGE_REGISTRY="registry:5000/$IMAGE_REPO" make manifests
     OCI_BIN="$OCI_BIN" IMAGE_REGISTRY="$IMG_REGISTRY" make img-build
     "$OCI_BIN" push --tls-verify=false "$IMG_REGISTRY/$IMAGE_NAME:$IMAGE_TAG"
 }
 
-publish_kubeconfig() {
-    local kube_config_dir="${HOME}/.kube/"
-    mkdir -p "$kube_config_dir"
-    cp kubevirtci/_ci-configs/k8s-1.24/.kubeconfig "$kube_config_dir/config"
-    echo "###"
-    echo "   Repo kubeconfig moved to $kube_config_dir/config"
-    echo "###"
+setup_registry() {
+  # https://minikube.sigs.k8s.io/docs/handbook/registry/
+  minikube addons enable registry
+  "$OCI_BIN" run --rm -it --network=host alpine ash -c "apk add socat && socat TCP-LISTEN:5000,reuseaddr,fork TCP:$(minikube ip):5000"
 }
 
 cleanup() {
-    rm -rf kubevirtci
     git checkout -- manifests/
+    echo "=============="
+    echo "  minikube logs:"
+    echo "=============="
+    minikube logs
 }
 
 trap "cleanup" EXIT
-setup_cluster
 
-registry_port=$(kubevirtci/cluster-up/cli.sh ports registry | tr -d '\r')
-registry="localhost:$registry_port/$IMAGE_REPO"
+setup_registry
 
-IMG_REGISTRY="$registry" push_local_image
+IMG_REGISTRY="localhost:5000/$IMAGE_REPO" push_local_image
 
 kubectl apply -f https://raw.githubusercontent.com/k8snetworkplumbingwg/multus-cni/master/deployments/multus-daemonset-thick.yml
 kubectl wait -nkube-system --for=condition=ready --timeout=180s -l app=multus pods
-
-publish_kubeconfig
 
 kubectl apply -f manifests/dynamic-networks-controller.yaml
 kubectl wait -nkube-system --for=condition=ready --timeout=180s -l app=dynamic-networks-controller pods
