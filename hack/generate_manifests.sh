6ab9ab5b8f24544ae608472a2507c0bc84735a25
Miguel Duarte Barroso <mdbarroso@redhat.com>
2022-10-06 16:32:55 +0200

    crio, deploy: provide manifests to deploy controller for this runtime
    This PR changes the manifest templating to also provide a manifest for
    CRIO based installations.
    
    This manifest features a different runtime configuration, which will
    cause the controller to use a different container runtime to access the
    pod's sandbox ID, and namespace.
    
    The golang code is also updated to use this new runtime, when
    configured.
    
    Signed-off-by: Miguel Duarte Barroso <mdbarroso@redhat.com>

--- hack/generate_manifests.sh
+++ hack/generate_manifests.sh
@@ -7,7 +7,11 @@ templates_dir="$ROOT/templates"
 
 for file in `ls $templates_dir/`; do
 	echo $file
-	j2 -e IMAGE_REGISTRY -e IMAGE_TAG -e CRI_SOCKET_PATH -e NAMESPACE ${templates_dir}/$file -o manifests/${file%.j2}
+	if [ -z $CRIO_RUNTIME ]; then
+	  j2 -e IMAGE_REGISTRY -e IMAGE_TAG -e CRI_SOCKET_PATH -e NAMESPACE ${templates_dir}/$file -o "manifests/${file%.j2}"
+	else
+	  j2 -e CRIO_RUNTIME -e IMAGE_REGISTRY -e IMAGE_TAG -e CRI_SOCKET_PATH -e NAMESPACE ${templates_dir}/$file -o "manifests/crio-${file%.j2}"
+	fi
 done
 unset IMAGE_REGISTRY
 unset IMAGE_TAG
