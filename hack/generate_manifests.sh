2d231aa0d56d8ce7eb286524993e578704bce870
Miguel Duarte Barroso <mdbarroso@redhat.com>
2022-11-29 16:17:27 +0100

    multus: liveness check multus
    Signed-off-by: Miguel Duarte Barroso <mdbarroso@redhat.com>

--- hack/generate_manifests.sh
+++ hack/generate_manifests.sh
@@ -8,15 +8,16 @@ templates_dir="$ROOT/templates"
 for file in `ls $templates_dir/`; do
 	echo $file
 	if [ -z $CRIO_RUNTIME ]; then
-	  j2 -e IMAGE_REGISTRY -e IMAGE_TAG -e CRI_SOCKET_PATH -e NAMESPACE ${templates_dir}/$file -o "manifests/${file%.j2}"
+	  j2 -e MULTUS_SOCKET_PATH -e IMAGE_REGISTRY -e IMAGE_TAG -e CRI_SOCKET_PATH -e NAMESPACE ${templates_dir}/$file -o "manifests/${file%.j2}"
 	else
 	  if [ $file != "dynamic-networks-controller.yaml.j2" ]; then
 	    continue
 	  fi
-	  j2 -e CRIO_RUNTIME -e IMAGE_REGISTRY -e IMAGE_TAG -e CRI_SOCKET_PATH -e NAMESPACE ${templates_dir}/$file -o "manifests/crio-${file%.j2}"
+	  j2 -e MULTUS_SOCKET_PATH -e CRIO_RUNTIME -e IMAGE_REGISTRY -e IMAGE_TAG -e CRI_SOCKET_PATH -e NAMESPACE ${templates_dir}/$file -o "manifests/crio-${file%.j2}"
 	fi
 done
 unset IMAGE_REGISTRY
 unset IMAGE_TAG
 unset CRI_SOCKET_PATH
 unset NAMESPACE
+unset MULTUS_SOCKET_PATH
