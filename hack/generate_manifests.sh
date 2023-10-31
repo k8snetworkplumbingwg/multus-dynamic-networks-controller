98d3beaff4d76e76b1efd64d028cdea9b631b6b8
Felix Matouschek <fmatouschek@redhat.com>
2023-10-31 13:58:13 +0100

    fix: Replace j2cli with jinjanator in GitHub workflows
    j2cli is unmaintained and no longer works with python3.12. It is
    therefore replaced with an active fork called jinjanator.
    
    The improper use of the '-e' flag on the j2 binary is fixed. Since the
    env is used as data source there is also no need to import variables
    explicitly again.
    
    Signed-off-by: Felix Matouschek <fmatouschek@redhat.com>

--- hack/generate_manifests.sh
+++ hack/generate_manifests.sh
@@ -8,12 +8,12 @@ templates_dir="$ROOT/templates"
 for file in `ls $templates_dir/`; do
 	echo $file
 	if [ -z $CRIO_RUNTIME ]; then
-	  j2 -e MULTUS_SOCKET_PATH -e IMAGE_REGISTRY -e IMAGE_TAG -e CRI_SOCKET_PATH -e NAMESPACE ${templates_dir}/$file -o "manifests/${file%.j2}"
+	  j2 ${templates_dir}/$file -o "manifests/${file%.j2}"
 	else
 	  if [ $file != "dynamic-networks-controller.yaml.j2" ]; then
 	    continue
 	  fi
-	  j2 -e MULTUS_SOCKET_PATH -e CRIO_RUNTIME -e IMAGE_REGISTRY -e IMAGE_TAG -e CRI_SOCKET_PATH -e NAMESPACE ${templates_dir}/$file -o "manifests/crio-${file%.j2}"
+	  j2  ${templates_dir}/$file -o "manifests/crio-${file%.j2}"
 	fi
 done
 unset IMAGE_REGISTRY
